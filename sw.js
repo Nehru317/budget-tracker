4d8149d0f1e686b9002ae7f0d409e2e2b8968fca
Nehru317 <Nehru317@users.noreply.github.com>
2026-09-07 08:06:38 +0400

    Keep the phone header below the iOS status bar.

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = "budget-tracker-v11";
+const CACHE = "budget-tracker-v12";
 const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./css/styles.css?v=11",
-  "./js/storage.js?v=11",
-  "./js/calc.js?v=11",
-  "./js/csv.js?v=11",
-  "./js/app.js?v=11",
+  "./css/styles.css?v=12",
+  "./js/storage.js?v=12",
+  "./js/calc.js?v=12",
+  "./js/csv.js?v=12",
+  "./js/app.js?v=12",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/apple-touch-icon.png",
